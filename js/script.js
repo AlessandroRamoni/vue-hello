8b2176ada8d8f74706f8f74c29030d21b3e89a8d
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-13 09:52:20 +0200

    add :alt

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const app = new Vue({
   el: "#container",
   data: {
     messaggio: "London calling",
+    altDescrizione: "modifica dell'alt tramite Vue",
     stile: "h1",
     immagine: "./img/03.jpg",
   },
+  methods: {},
 });
